c9862dd10e8ab1d2e6a4ce48a893068256b683c2
AHYEON04 <ayeon3590@gmail.com>
2021-05-20 16:57:56 +0900

    Update PartialType

--- hi-nest/src/movies/dto/update-movie.dto.ts
+++ hi-nest/src/movies/dto/update-movie.dto.ts
@@ -1,10 +1,8 @@
 import {IsNumber, IsString} from "class-validator"
+import{PartialType} from "@nestjs/mapped-types"
+import{fromEventPattern} from 'rxjs';
+import { CreateMovieDto } from "./create-movie.dto";
 
-export class UpdateMovieDto{
-    @IsString()
-    readonly title?: string;
-    @IsNumber()
-    readonly year?: number;
-    @IsString({each:true})
-    readonly genres?:string[];
+export class UpdateMovieDto extends PartialType(CreateMovieDto){
+    
 }
